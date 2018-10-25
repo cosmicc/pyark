426c66984150f122560ce89fc3af05650bcd93a9
root <ianperry99@gmail.com>
2018-10-25 23:56:50 +0000

    update

--- test.py
+++ test.py
@@ -1,58 +1,16 @@
 import sqlite3
-from datetime import datetime
 from configreader import statsdb
+import pandas as pd
 
-
-def checkiftableexists(inst):
-    conn = sqlite3.connect(statsdb)
-    c = conn.cursor()
-    c.execute('CREATE TABLE IF NOT EXISTS %s (date TEXT, value INTEGER)' % (inst,))
-    conn.commit()
-    c.close()
-    conn.close()
-
-
-def checkifinstexistsOLD(inst):
-    conn = sqlite3.connect(statsdb)
-    c = conn.cursor()
-    c.execute('PRAGMA table_info({})'.format('online'))
-    alldata = c.fetchall()
-    c.close()
-    conn.close()
-    tot = 0
-    for f in alldata:
-        if f[1] == inst:
-            tot += 1
-    if tot == 0:
-        conn = sqlite3.connect(statsdb)
-        c = conn.cursor()
-        c.execute('ALTER TABLE online ADD COLUMN %s TEXT' % (inst,))
-        conn.commit()
-        c.close()
-        conn.close()
-
-
-def addvalue(inst, value):
-    now = datetime.now()
-    ldate = now.strftime("%y-%m-%d,")
-    print(ldate)
-    conn = sqlite3.connect(statsdb)
-    c = conn.cursor()
-    c.execute('INSERT INTO %s (date, value) VALUES (%s, %s)' % (inst, ldate, value))
-    conn.commit()
-    c.close()
-    conn.close()
-
-
-checkiftableexists('ragnarok')
-addvalue('ragnarok', 3)
 conn = sqlite3.connect(statsdb)
-c = conn.cursor()
-c.execute('SELECT * FROM ragnarok')
-alldata = c.fetchall()
-c.close()
-conn.close()
-print(alldata)
 
+df = pd.read_sql('SELECT * FROM ragnarok', conn, parse_dates=['date'], index_col='date')
+
+conn.close()
+
+# Daily Averages
+df = df.tz_localize(tz='UTC')
+df = df.tz_convert(tz='US/Eastern')
+print(df.resample('H').mean())
 
 
