4cd66e191fa3b51dc2279181ec7528db3f9bea05
root <ianperry99@gmail.com>
2018-11-14 04:21:09 +0000

    update

--- webui/webui.py
+++ webui/webui.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, Response, request
+from active_alchemy import ActiveAlchemy
+from flask import Flask, render_template, Response, request, redirect, url_for
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
 import sys
 sys.path.append('/home/ark/pyark')
-from modules.configreader import webui_ip, webui_port, webui_debug
-from modules.dbhelper import dbquery
+from modules.configreader import webui_ip, webui_port, webui_debug, psql_host, psql_port, psql_user, psql_pw, psql_db, psql_statsdb
 from modules.instances import instancelist, isinstanceup, isinrestart, restartinstance, getlog, iscurrentconfig
 from modules.players import getplayersonline, getlastplayersonline, isplayerbanned, getplayer, banunbanplayer, isplayeronline, isplayerold, kickplayer
 from modules.timehelper import elapsedTime, Now, playedTime, epochto, Secs
@@ -10,7 +14,27 @@ from lottery import isinlottery, getlotteryplayers, getlotteryendtime
 import json
 sys.path.append('/home/ark/pyark/webui')
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'fuckoff'
 
+db = ActiveAlchemy(f"postgresql+pg8000://{psql_user}:{psql_pw}@{psql_host}:{psql_port}/{psql_db}", app=app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+class users(UserMixin, db.Model):
+    username = db.Column(db.String(15), unique=True)
+    password = db.Column(db.String(80))
+    email = db.Column(db.String(50), unique=True)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return users.get(int(user_id))
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField('remember me')
 
 @app.context_processor
 def database_processor():
@@ -235,38 +259,65 @@ def manifest():
     return Response(data, mimetype='application/x-web-app-manifest+json')
 
 
-@app.route('/')
+@app.route('/dashboard')
+@login_required
 def dashboard():
-    return render_template('dashboard.html', instances=instancelist())
+    return render_template('dashboard.html', loginname=current_user.username, instances=instancelist())
+
+
+@app.route('/', methods=['POST', 'GET'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = users.query().filter_by(username=form.username.data).first()
+        if user:
+            if form.password.data == user.password:
+                login_user(user, remember=form.remember.data)
+                return redirect(url_for('dashboard'))
+        return render_template('login.html', alertmessage='Invalid Login')
+
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 
 @app.route('/serverinfo/<instance>')
+@login_required
 def serverinfo(instance):
     return render_template('serverinfo.html', serverinfo=instanceinfo(instance))
 
 
 @app.route('/playersearch', methods=['POST', 'GET'])
+@login_required
 def result():
     if request.method == 'POST':
         return render_template("playerinfo.html", playerinfo=getplayer(playername=request.form['player'].lower(), fmt='dict'))
 
 
 @app.route('/playerinfo/<player>')
+@login_required
 def playerinfo(player):
     return render_template('playerinfo.html', playerinfo=getplayer(playername=player.lower(), fmt='dict'))
 
 
 @app.route('/playerinfo')
+@login_required
 def _players():
     return render_template('playerselect.html', players=getplayernames(), bannedplayers=getbannedplayers(), expiredplayers=getexpiredplayers(), newplayers=getnewplayers('week'))
 
 
 @app.route('/events')
+@login_required
 def _events():
     return render_template('eventinfo.html', lastevent=getlastevent(), currentevent=getcurrentevent(), futureevent=getfutureevent())
 
 
 @app.route('/bantoggle/<steamid>')
+@login_required
 def _bantoggle(steamid):
     if isplayerbanned(steamid=steamid):
         banunbanplayer(steamid=steamid, ban=False)
@@ -277,28 +328,31 @@ def _bantoggle(steamid):
 
 
 @app.route('/kickplayer/<steamid>/<instance>')
+@login_required
 def _kickplayer(steamid, instance):
     kickplayer(instance, steamid)
     return render_template('playerinfo.html', playerinfo=getplayer(steamid=steamid, fmt='dict'), alertmessage='has been kicked from server')
 
 
 @app.route('/server/restart/<instance>')
+@login_required
 def _restartinstance(instance):
     restartinstance(instance, cancel=False)
     return render_template('serverinfo.html', serverinfo=instanceinfo(instance), alertmessage='server is set to restart')
 
 
 @app.route('/server/cancelrestart/<instance>')
+@login_required
 def _cancelrestartinstance(instance):
     restartinstance(instance, cancel=True)
     return render_template('serverinfo.html', serverinfo=instanceinfo(instance), alertmessage='server restart canceled')
 
 
 @app.route('/server/chatlog/<instance>')
+@login_required
 def _chatlog(instance):
     chatlog = getlog(instance, 'chat')
     return render_template('serverchatlog.html', serverinfo=instanceinfo(instance), chatlog=chatlog[::-1])
 
-
 if __name__ == '__main__':
-    app.run(host=webui_ip, port=webui_port, debug=webui_debug)
+    app.run(host=webui_ip, port=51501, debug=webui_debug)
