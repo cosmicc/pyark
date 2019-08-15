7589ef00d2f37d407ab7096c072f3febc4d7b944
root <ianperry99@gmail.com>
2019-08-15 01:22:33 +0000

    update

--- gchatrelay.py
+++ gchatrelay.py
@@ -2,6 +2,7 @@ from modules.dbhelper import dbupdate, db_getall
 from modules.players import getplayer
 from modules.timehelper import Now
 from cmdlistener import writechatlog
+from modules.instances import writechat
 from time import sleep
 from loguru import logger as log
 import subprocess
@@ -19,24 +20,28 @@ def gchatrelay(inst):
                     if msg['server'] == 'ALERT':
                         msg['server'] == 'ALL'
 
-                    if msg['server'] == 'ALL' or msg['server'].lower() == inst and float(msg['timestamp']) > Now() - 3:
-                        if not msg['private'] and not msg['broadcast']:
+                    if (msg['server'] == 'ALL' or msg['server'].lower() == inst) and not Now() - float(msg['timestamp']) > 3:
+                        if not msg['private'] and not msg['broadcast'] and not Now() - float(msg['timestamp']) > 3:
                             subprocess.run('arkmanager rconcmd "ServerChat %s" @%s' % (f"Admin: {msg['message']}", inst), shell=True)
                             log.log('CHAT', f'{inst} | ADMIN | {msg["message"]}')
                             writechatlog(inst, 'ADMIN', msg['message'], Now())
+                            writechat(inst, '*Admin', msg['message'], Now())
 
-                        elif msg['broadcast'] and not msg['private']:
+                        elif msg['broadcast'] and not msg['private'] and not Now() - float(msg['timestamp']) > 3:
                             subprocess.run('arkmanager rconcmd "Broadcast %s" @%s' % (msg['message'], inst), shell=True)
                             log.log('CHAT', f'{inst} | BROADCAST | {msg["message"]}')
                             writechatlog(inst, 'BROADCAST', msg['message'], Now())
+                            writechat(inst, 'Broadcast', msg['message'], Now())
 
-                        elif msg['private'] and not msg['broadcast']:
+                        elif msg['private'] and not msg['broadcast'] and not Now() - float(msg['timestamp']) > 3:
                             cplayer = getplayer(playername=msg['name'], fmt='dict')
                             if cplayer:
                                 if cplayer['server'] == inst:
+                                    log.log('CHAT', f'{inst} | ADMINPRIVATE | {msg["message"]}')
+                                    writechatlog(inst, 'ADMIN', msg['message'], Now())
                                     subprocess.run("""arkmanager rconcmd 'ServerChatTo "%s" AdminPrivate: %s' @%s""" % (cplayer['steamid'], msg['message'], inst), shell=True)
 
-                    #elif msg['server'] != inst and msg['name'] != 'ALERT' and float(msg['timestamp']) > Now() - 3:
+                    # elif msg['server'] != inst and msg['name'] != 'ALERT' and float(msg['timestamp']) > Now() - 3:
                     #    log.trace(f'Server chat: msg["server"].capitalize() - msg["name"].title() -  msg["message"]')
                     #    subprocess.run('arkmanager rconcmd "ServerChat %s@%s: %s" @%s' % (msg['name'].title(), msg['server'].capitalize(), msg['message'], inst), shell=True)
 
