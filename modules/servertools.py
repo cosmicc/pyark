3b20eca1ccc5a79c6ef9975e62741d3490ae675c
root <ianperry99@gmail.com>
2019-08-21 20:45:22 +0000

    update

--- modules/servertools.py
+++ modules/servertools.py
@@ -5,7 +5,7 @@ def serverexec(cmdlist, nice=10, null=False):
     if type(cmdlist) is not list:
         raise TypeError
     else:
-        fullcmdlist = ['/usr/bin/nice', '-n', nice] + cmdlist
+        fullcmdlist = ['/usr/bin/nice', '-n', str(nice)] + cmdlist
     if null:
         sproc = subprocess.run(fullcmdlist, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=False)
         return sproc['returncode']
