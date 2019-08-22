b56b16fd1bd637f3c4ea11e3fb2f835b2554f318
root <ianperry99@gmail.com>
2019-08-22 03:03:42 +0000

    update

--- cmdlistener.py
+++ cmdlistener.py
@@ -592,14 +592,14 @@ def processgameline(inst, ltype, line):
 
 def playerjoin(line, inst):
     newline = line[:-17].split(':')
-    player = dbquery("SELECT * FROM players WHERE steamname = '%s'" % (newline[1].strip()), single=True, fmt='dict')
+    player = dbquery("SELECT * FROM players WHERE steamname = '%s'" % (newline[1].strip()), single=True, fmt='dict', fetch='one')
     if player:
         log.log('JOIN', f'Player [{player["playername"].title()}] has joined [{inst.title()}]')
 
 
 def playerleave(line, inst):
     newline = line[:-15].split(':')
-    player = dbquery("SELECT * FROM players WHERE steamname = '%s'" % (newline[1].strip()), single=True, fmt='dict')
+    player = dbquery("SELECT * FROM players WHERE steamname = '%s'" % (newline[1].strip()), single=True, fmt='dict', fetch='one')
     if player:
         log.log('LEAVE', f'Player [{player["playername"].title()}] has left [{inst.title()}]')
 
