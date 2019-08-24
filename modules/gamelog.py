431952ebebf9224e28b80528abcbd7ac263d908f
root <ianperry99@gmail.com>
2019-08-24 18:51:36 +0000

    update

--- modules/gamelog.py
+++ modules/gamelog.py
@@ -1,9 +1,12 @@
 from loguru import logger as log
 from modules.timehelper import Now
 from modules.servertools import removerichtext
+import re
 
 
+@log.catch
 def processgameline(inst, ptype, line):
+    try:
         clog = log.patch(lambda record: record["extra"].update(instance=inst))
         logheader = f'{Now(fmt="dt").strftime("%a %I:%M%p")}|{inst.upper():>8}|{ptype:<7}| '
         linesplit = removerichtext(line[21:]).split(", ")
@@ -40,6 +43,7 @@ def processgameline(inst, ptype, line):
                     # wglog(inst, f'{Now(fmt="string")}: [{playername.title()}] {playerlevel} was killed by [{killedby}]')
                 elif deathsplit[1].find('killed!') != -1:
                     clog.log(ptype, f'{logheader}[{playername.title()}] has died')
+                    log.debug(f'dt: {deathsplit}')
                     # wglog(inst, f'{Now(fmt="string")}: [{playername.title()}] has died')
                 else:
                     log.warning(f'not found gameparse death: {deathsplit}')
@@ -57,3 +61,21 @@ def processgameline(inst, ptype, line):
                         clog.log(ptype, f'{logheader}[{tribename}] tamed [{tamed}]')
                     else:
                         clog.log(ptype, f'{logheader}[{playername.title()}] of ({tribename}) tamed [{tamed}]')
+            else:
+                log.info(f'TAME: {linesplit}')
+                clog.log(ptype, f'TAME: {linesplit}')
+        elif ptype == 'DECAY':
+            log.debug(f'{inst}, {ptype}, {linesplit}')
+            clog.log(ptype, f'{line} ## {linesplit}')
+            tribename = linesplit[0][6:].strip()
+            tribeid = linesplit[1].split(':')[0][3:].strip()
+            decayitem = linesplit[2].split("'", 1)[1].split("'")[0]
+            # decayitem = re.search('\(([^)]+)', linesplit[2]).group(1)
+            clog.log(ptype, f'{tribename} {decayitem}')
+            # wglog(inst, removerichtext(line[21:]))
+        else:
+            log.debug(f'{inst}, {ptype}, {linesplit}')
+            clog.log(ptype, f'{line} ## {linesplit}')
+            # wglog(inst, removerichtext(line[21:]))
+    except:
+        log.critical(f'GAME LOG ERROR IN LINE: {line} ## {linesplit}')
