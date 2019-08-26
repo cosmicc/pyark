c5ac1c73a0d31c208320c1631a0bd32c840b1604
root <ianperry99@gmail.com>
2019-08-26 02:53:53 +0000

    update

--- cmdlistener.py
+++ cmdlistener.py
@@ -765,7 +765,7 @@ def checkcommands(minst):
                     lend = elapsedTime(datetimeto(lottoinfo['startdate'] + timedelta(hours=lottoinfo['days']), fmt='epoch'), Now())
 
                     bcast = f"""Broadcast <RichColor Color="0.0.0.0.0.0"> </>\n\n<RichColor Color="0,1,0,1">           A new lottery has started! {lottoinfo["buyin"]} points to enter in this lottery</>\n\n<RichColor Color="1,1,0,1">    Starting pot {lottoinfo["payout"]} points and grows as players enter\n<RichColor Color="1,1,0,1">            Lottery Ends in {lend}\nType !lotto for more info or !lotto enter to join</>"""
-                    serverexec(['arkmanager', 'rconcmd', '''{bcast}''', f'@{inst}'], nice=19, null=True)
+                    serverexec(['arkmanager', 'rconcmd', f'''{bcast}''', f'@{inst}'], nice=19, null=True)
              
 
                 elif line.lower().find('!mypoints') != -1 or line.lower().find('!myinfo') != -1:
