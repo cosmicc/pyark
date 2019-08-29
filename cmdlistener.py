552166f4f60e07905d6d5aa50d4f6fad97a2e6c2
root <ianperry99@gmail.com>
2019-08-29 04:06:40 +0000

    update

--- cmdlistener.py
+++ cmdlistener.py
@@ -30,8 +30,8 @@ async def asyncserverexec(cmdlist, nice):
         fullcmdlist = ['/usr/bin/nice', '-n', str(nice)] + cmdlist
         cmdstring = quote(' '.join(fullcmdlist))
         log.debug(f'server rcon cmd executing {cmdstring}')
-        proc = asyncio.create_subprocess_shell(cmdstring, loop=asyncloop)
-        await asyncio.wait_for(proc, timeout=10, loop=asyncloop)
+        #proc = asyncio.create_subprocess_shell(cmdstring, loop=asyncloop)
+        #await asyncio.wait_for(proc, timeout=10, loop=asyncloop)
         log.debug(f'server rcon process completed {cmdlist}')
     except:
         log.exception('shitty')
