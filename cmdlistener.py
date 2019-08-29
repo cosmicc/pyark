85e9b7174300b0d62d1c4113e4dbfcf44701d67b
root <ianperry99@gmail.com>
2019-08-29 05:28:05 +0000

    update

--- cmdlistener.py
+++ cmdlistener.py
@@ -902,7 +902,6 @@ async def processline(minst, line):
 async def checkcommands(inst, dtime):
     while True:
         starttime = Now()
-        log.debug('.')
         cmdpipe = serverexec(['arkmanager', 'rconcmd', 'getgamelog', f'@{inst}'], nice=5, null=False)
         b = cmdpipe.stdout.decode("utf-8")
         for line in iter(b.splitlines()):
