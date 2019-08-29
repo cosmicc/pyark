278794bb94e032b033b11e08f55a585a13a63d15
root <ianperry99@gmail.com>
2019-08-29 04:08:24 +0000

    update

--- cmdlistener.py
+++ cmdlistener.py
@@ -26,12 +26,12 @@ arewevoting = False
 @log.catch
 async def asyncserverexec(cmdlist, nice):
     global asyncloop
-        fullcmdlist = ['/usr/bin/nice', '-n', str(nice)] + cmdlist
-        cmdstring = quote(' '.join(fullcmdlist))
-        log.debug(f'server rcon cmd executing {cmdstring}')
-        #proc = asyncio.create_subprocess_shell(cmdstring, loop=asyncloop)
-        #await asyncio.wait_for(proc, timeout=10, loop=asyncloop)
-        log.debug(f'server rcon process completed {cmdlist}')
+    fullcmdlist = ['/usr/bin/nice', '-n', str(nice)] + cmdlist
+    cmdstring = quote(' '.join(fullcmdlist))
+    log.debug(f'server rcon cmd executing {cmdstring}')
+    #proc = asyncio.create_subprocess_shell(cmdstring, loop=asyncloop)
+    #await asyncio.wait_for(proc, timeout=10, loop=asyncloop)
+    log.debug(f'server rcon process completed {cmdlist}')
 
 
 @log.catch
