0ccfc59697f24906fa82d8f3a0a236f478ef8349
root <ianperry99@gmail.com>
2019-09-02 03:11:04 +0000

    update

--- modules/onlinemonitor.py
+++ modules/onlinemonitor.py
@@ -269,8 +269,8 @@ async def asynconlineupdate(inst, dtime, stop_event):
         while time.time() - liststart < dtime:
             if time.time() - kickstart < 5:
                 kicker = asyncloop.create_task(asynckicker(inst))
-                await kicker
-                kickstart = time.time()
+                #await kicker
+                #kickstart = time.time()
             await asyncio.sleep(1)
     pendingtasks = asyncio.Task.all_tasks()
     await asyncio.gather(*pendingtasks)
