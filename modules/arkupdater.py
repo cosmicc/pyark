7fee47c460196fd7744b20bd248dec697f1404ba
root <ianperry99@gmail.com>
2019-09-07 17:52:15 +0000

    update

--- modules/arkupdater.py
+++ modules/arkupdater.py
@@ -147,7 +147,7 @@ async def asynccheckwipe(instances):
         lastwipe = await asyncgetlastwipe(inst)
         if Now() - lastwipe > Secs['12hour'] and await asyncisinstanceup(inst):
             oplayers = await asyncgetliveplayersonline(inst)
-            if oplayers['activeplayers'] == 0 and len(await asyncgetplayersonline()) == 0:
+            if oplayers['activeplayers'] == 0 and len(await asyncgetplayersonline(inst)) == 0:
                 log.log('WIPE', f'Dino wipe needed for [{inst.title()}], server is empty, wiping now')
                 await asyncwritechat(inst, 'ALERT', f'### Empty server is over 12 hours since wild dino wipe. Wiping now.', wcstamp())
                 asyncio.create_task(asyncwipeit(inst))
