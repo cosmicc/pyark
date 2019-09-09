acce947031220883a3d247247fce3106f811b081
root <ianperry99@gmail.com>
2019-09-09 23:14:54 +0000

    update

--- modules/callbackclasses.py
+++ modules/callbackclasses.py
@@ -13,25 +13,23 @@ class DFProtocol(asyncio.SubprocessProtocol):
         super().__init__()
 
     def connection_made(self, transport):
-        log.info('process started {}'.format(transport.get_pid()))
+        log.trace('process started {}'.format(transport.get_pid()))
         self.transport = transport
 
     def pipe_data_received(self, fd, data):
-        log.success(f'read {len(data)} bytes from {self.FD_NAMES[fd]}')
+        log.trace(f'read {len(data)} bytes from {self.FD_NAMES[fd]}')
         if fd == 1:
             self._parse_results(data)
 
     def process_exited(self):
-        log.info('process exited')
+        log.trace('process exited')
         return_code = self.transport.get_returncode()
-        log.info('return code {}'.format(return_code))
+        log.trace('return code {}'.format(return_code))
         asyncio.create_task(asyncfinishstatus(self.inst))
         self.done.set_result((return_code))
 
     def _parse_results(self, line):
-        log.info('parsing results')
+        log.trace('parsing results')
         if not line:
             return []
-        log.info(f'LINE: {line}')
         asyncio.create_task(asyncprocessstatusline(self.inst, line))
-        # return results
