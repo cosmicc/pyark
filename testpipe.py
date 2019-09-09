559ebb13c70174d78c0cf3e68df49f5a789ec78e
root <ianperry99@gmail.com>
2019-09-09 21:48:46 +0000

    update

--- testpipe.py
+++ testpipe.py
@@ -59,7 +59,7 @@ async def asyncmain():
     asyncloop.set_exception_handler(async_exception_handler)
     # while not main_stop_event:
     asyncio.create_task(runcmd('arkmanager', 'status', '@ragnarok'))
-    await asyncio.sleep(2)
+    #await asyncio.sleep(2)
     # tasks = [t for t in asyncio.all_tasks() if t is not asyncio.current_task()]
     # log.debug(f'Waiting for {len(tasks)} async tasks to finish')
     # await asyncio.gather(*tasks, return_exceptions=True)
