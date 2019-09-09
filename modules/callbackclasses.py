bd13c438b35016ecd37c3eb1058e0f46c2132373
root <ianperry99@gmail.com>
2019-09-09 21:58:14 +0000

    update

--- modules/callbackclasses.py
+++ modules/callbackclasses.py
@@ -8,7 +8,6 @@ class DFProtocol(asyncio.SubprocessProtocol):
 
     def __init__(self, done_future):
         self.done = done_future
-        self.buffer = bytearray()
         super().__init__()
 
     def connection_made(self, transport):
@@ -18,26 +17,17 @@ class DFProtocol(asyncio.SubprocessProtocol):
     def pipe_data_received(self, fd, data):
         log.success(f'read {len(data)} bytes from {self.FD_NAMES[fd]}')
         if fd == 1:
-            print(data)
-            self.buffer.extend(data)
+            self._parse_results(self, data)
 
     def process_exited(self):
         log.info('process exited')
         return_code = self.transport.get_returncode()
         log.info('return code {}'.format(return_code))
-        cmd_output = bytes(self.buffer).decode()
-        results = self._parse_results(cmd_output)
-        self.done.set_result((return_code, results))
+        self.done.set_result((return_code))
 
-    def _parse_results(self, output):
+    def _parse_results(self, line):
         log.info('parsing results')
-        # Output has one row of headers, all single words.  The
-        # remaining rows are one per filesystem, with columns
-        # matching the headers (assuming that none of the
-        # mount points have whitespace in the names).
-        if not output:
+        if not line:
             return []
-        lines = output.splitlines()
-        log.info(f'LINES: {lines}')
-        results = [line for line in lines]
-        return results
+        log.info(f'LINE: {line}')
+        # return results
