250514d988570412743f116aad13503f46ffa61a
root <ianperry99@gmail.com>
2019-09-10 01:57:02 +0000

    update

--- modules/onlinemonitor.py
+++ modules/onlinemonitor.py
@@ -236,7 +236,7 @@ async def asynconlinedblchecker(instances):
 @log.catch
 async def asyncprocessonline(inst, eline):
     line = eline.decode()
-    print('.')
+    print(f'# {line}')
     if line.startswith(('Running command', '"', ' "', 'Error:', '"No Players')):
         pass
     else:
