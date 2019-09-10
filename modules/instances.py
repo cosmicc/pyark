1ba3da3108f95d425587d5f70a882023eff41a92
root <ianperry99@gmail.com>
2019-09-10 03:02:41 +0000

    update

--- modules/instances.py
+++ modules/instances.py
@@ -98,7 +98,7 @@ async def asyncfinishstatus(inst):
 
 async def asyncprocessstatusline(inst, eline):
         line = eline.decode()
-        print(f'# {eline}')
+        print(repr(stripansi(line)))
         status_title = stripansi(line.split(':')[0]).strip()
         if not status_title.startswith('Running command'):
             status_value = stripansi(line.split(':')[1]).strip()
