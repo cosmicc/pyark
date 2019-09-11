d747ef8a3207d64d4ef99c3303a39cced6365874
root <ianperry99@gmail.com>
2019-09-11 04:44:26 +0000

    update

---
+++ arkwatchdog.py
@@ -0,0 +1,17 @@
+import subprocess
+
+import redis
+
+from modules.configreader import hstname, redis_host, redis_port
+
+r = redis.Redis(host=redis_host, port=redis_port, db=0)
+pubsub = r.pubsub()
+pubsub.subscribe([f'{hstname.lower()}-commands'])
+for response in pubsub.listen():
+    if response['type'] == 'message' and response['channel'].decode() == f'{hstname.lower()}-commands':
+        if response['data'].decode() == 'restart':
+            subprocess.run(['systemctl', 'restart', 'pyark'], shell=False, capture_output=False)
+        elif response['data'].decode() == 'stop':
+            subprocess.run(['systemctl', 'stop', 'pyark'], shell=False, capture_output=False)
+        elif response['data'].decode() == 'start':
+            subprocess.run(['systemctl', 'start', 'pyark'], shell=False, capture_output=False)
