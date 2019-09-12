acd5e6dc7be7d49359c16b19c9c7d4cdb9df1946
Ian Perry <ianperry99@gmail.com>
2019-09-12 19:51:25 -0400

    update

--- modules/servertools.py
+++ modules/servertools.py
@@ -1,3 +1,8 @@
+"""Summary
+
+Attributes:
+    redis (TYPE): Description
+"""
 import asyncio
 import subprocess
 from functools import partial
@@ -16,53 +21,168 @@ redis = Redis.redis
 
 
 class instvar:
+
+    """Summary
+    """
+
     def __init__(self):
+        """Summary
+        """
         pass
 
     async def set(self, inst, var, value):
+        """Summary
+
+        Args:
+            inst (TYPE): Description
+            var (TYPE): Description
+            value (TYPE): Description
+        """
         await redis.hset(f'{inst}', var, value)
 
     async def remove(self, inst, var):
+        """Summary
+
+        Args:
+            inst (TYPE): Description
+            var (TYPE): Description
+        """
         await redis.hdel(f'{inst}', var)
 
     async def get(self, inst, var):
+        """Summary
+
+        Args:
+            inst (TYPE): Description
+            var (TYPE): Description
+
+        Returns:
+            TYPE: Description
+        """
         return await redis.hget(f'{inst}', var)
 
     async def inc(self, inst, var):
+        """Summary
+
+        Args:
+            inst (TYPE): Description
+            var (TYPE): Description
+
+        Returns:
+            TYPE: Description
+        """
         return await redis.hincrby(f'{inst}', var, 1)
 
     async def dec(self, inst, var):
+        """Summary
+
+        Args:
+            inst (TYPE): Description
+            var (TYPE): Description
+
+        Returns:
+            TYPE: Description
+        """
         return await redis.hincrby(f'{inst}', var, -1)
 
     async def check(self, inst, var):
+        """Summary
+
+        Args:
+            inst (TYPE): Description
+            var (TYPE): Description
+
+        Returns:
+            TYPE: Description
+        """
         return await redis.hexists(f'{inst}', var)
 
 
 class inststate:
+
+    """Summary
+    """
+
     def __init__(self):
+        """Summary
+        """
         pass
 
     async def set(self, inst, state):
+        """Summary
+
+        Args:
+            inst (TYPE): Description
+            state (TYPE): Description
+        """
         await redis.sadd(f'{inst}-states', state)
 
     async def unset(self, inst, state):
+        """Summary
+
+        Args:
+            inst (TYPE): Description
+            state (TYPE): Description
+        """
         await redis.srem(f'{inst}-states', state)
 
     async def check(self, inst, state):
+        """Summary
+
+        Args:
+            inst (TYPE): Description
+            state (TYPE): Description
+
+        Returns:
+            TYPE: Description
+        """
         return redis.sismember(f'{inst}-states', state)
 
 
 def stripansi(stripstr):
+    """[summary]
+    Strip ANSI characters from a string
+    [description]
+
+    Arguments:
+        stripstr (TYPE): Description
+        stripstr {[string]} -- [string to reomove ANSI codes from]
+
+    Returns:
+        TYPE: Description
+    """
     ansi_escape = rcompile(r'\x1B\[[0-?]*[ -/]*[@-~]')
     return(ansi_escape.sub('', stripstr).strip())
 
 
 def filterline(stripstr):
+    """Summary
+
+    Args:
+        stripstr (TYPE): Description
+
+    Returns:
+        TYPE: Description
+    """
     return(stripansi(stripstr).replace('\n', '').replace('\r', '').replace('"', '').strip())
 
 
 def asynctimeit(func):
+    """Summary
+
+    Args:
+        func (TYPE): Description
+
+    Returns:
+        TYPE: Description
+    """
     async def wrapper(*args, **kwargs):
+        """Summary
+
+        Args:
+            *args: Description
+            **kwargs: Description
+        """
         asyncloop = asyncio.get_running_loop()
         astart_time = asyncloop.time()
         await func(*args, **kwargs)
@@ -72,11 +192,23 @@ def asynctimeit(func):
 
 @log.catch
 async def gettotaldbconnections():
+    """Summary
+
+    Returns:
+        TYPE: Description
+    """
     return await db.fetchone(f'SELECT count(*) FROM pg_stat_activity;')
 
 
 @log.catch
 async def asyncserverrconcmd(inst, command, nice=5):
+    """Summary
+
+    Args:
+        inst (TYPE): Description
+        command (TYPE): Description
+        nice (int, optional): Description
+    """
     cmdstring = f'/usr/bin/nice -n {nice} arkmanager rconcmd "{command}" @{inst}'
     proc = asyncio.create_subprocess_shell(cmdstring, stdout=None, stderr=None)
     asyncio.create_task(proc)
@@ -85,6 +217,13 @@ async def asyncserverrconcmd(inst, command, nice=5):
 
 @log.catch
 async def asyncserverscriptcmd(inst, command, nice=5):
+    """Summary
+
+    Args:
+        inst (TYPE): Description
+        command (TYPE): Description
+        nice (int, optional): Description
+    """
     cmdstring = f'/usr/bin/nice -n {nice} arkmanager rconcmd "ScriptCommand {command}" @{inst}'
     proc = asyncio.create_subprocess_shell(cmdstring, stdout=None, stderr=None)
     asyncio.create_task(proc)
@@ -93,6 +232,13 @@ async def asyncserverscriptcmd(inst, command, nice=5):
 
 @log.catch
 async def asyncserverchat(inst, message, nice=15):
+    """Summary
+
+    Args:
+        inst (TYPE): Description
+        message (TYPE): Description
+        nice (int, optional): Description
+    """
     cmdstring = f'/usr/bin/nice -n {nice} arkmanager rconcmd "ServerChat {message}" @{inst}'
     proc = asyncio.create_subprocess_shell(cmdstring, stdout=None, stderr=None)
     asyncio.create_task(proc)
@@ -101,6 +247,14 @@ async def asyncserverchat(inst, message, nice=15):
 
 @log.catch
 async def asyncserverchatto(inst, steamid, message, nice=15):
+    """Summary
+
+    Args:
+        inst (TYPE): Description
+        steamid (TYPE): Description
+        message (TYPE): Description
+        nice (int, optional): Description
+    """
     cmdstring = f"""/usr/bin/nice -n {nice} arkmanager rconcmd 'ServerChatTo "{steamid}" {message}' @{inst}"""
     proc = asyncio.create_subprocess_shell(cmdstring, stdout=None, stderr=None)
     asyncio.create_task(proc)
@@ -109,6 +263,13 @@ async def asyncserverchatto(inst, steamid, message, nice=15):
 
 @log.catch
 async def asyncserverbcast(inst, bcast, nice=10):
+    """Summary
+
+    Args:
+        inst (TYPE): Description
+        bcast (TYPE): Description
+        nice (int, optional): Description
+    """
     cmdstring = f"""/usr/bin/nice -n {nice} arkmanager rconcmd 'Broadcast {bcast}' @{inst}"""
     proc = asyncio.create_subprocess_shell(cmdstring, stdout=None, stderr=None)
     asyncio.create_task(proc)
@@ -117,6 +278,13 @@ async def asyncserverbcast(inst, bcast, nice=10):
 
 @log.catch
 async def asyncservernotify(inst, message, nice=10):
+    """Summary
+
+    Args:
+        inst (TYPE): Description
+        message (TYPE): Description
+        nice (int, optional): Description
+    """
     cmdstring = f"""/usr/bin/nice -n {nice} arkmanager notify "{message}" @{inst}"""
     proc = asyncio.create_subprocess_shell(cmdstring, stdout=None, stderr=None)
     asyncio.create_task(proc)
@@ -125,6 +293,17 @@ async def asyncservernotify(inst, message, nice=10):
 
 @log.catch
 async def asyncserverexec(cmdlist, nice=19, wait=False, _wait=False):
+    """Summary
+
+    Args:
+        cmdlist (TYPE): Description
+        nice (int, optional): Description
+        wait (bool, optional): Description
+        _wait (bool, optional): Description
+
+    Returns:
+        TYPE: Description
+    """
     fullcmdlist = ['/usr/bin/nice', '-n', str(nice)] + cmdlist
     cmdstring = ' '.join(fullcmdlist)
     if wait:
@@ -139,11 +318,24 @@ async def asyncserverexec(cmdlist, nice=19, wait=False, _wait=False):
 
 
 def removerichtext(text):
+    """Summary
+
+    Args:
+        text (TYPE): Description
+
+    Returns:
+        TYPE: Description
+    """
     return sub('<.*?>', '', text)
 
 
 @log.catch
 def serverneedsrestart():
+    """Summary
+
+    Returns:
+        TYPE: Description
+    """
     if isfile('/run/reboot-required'):
         return True
     else:
@@ -151,10 +343,23 @@ def serverneedsrestart():
 
 
 def issharedmounted():
+    """Summary
+
+    Returns:
+        TYPE: Description
+    """
     return globvars.sharepath.is_mount()
 
 
 def float_trunc_1dec(num):
+    """Summary
+
+    Args:
+        num (TYPE): Description
+
+    Returns:
+        TYPE: Description
+    """
     try:
         tnum = num // 0.1 / 10
     except:
@@ -164,6 +369,14 @@ def float_trunc_1dec(num):
 
 
 def getinstpid(inst):
+    """Summary
+
+    Args:
+        inst (TYPE): Description
+
+    Returns:
+        TYPE: Description
+    """
     try:
         return globvars.instpidfiles[inst].read_text()
     except FileNotFoundError:
@@ -174,16 +387,31 @@ def getinstpid(inst):
 
 
 async def getopenfiles():
+    """Summary
+
+    Returns:
+        TYPE: Description
+    """
     result = await asyncserverexec(['sysctl', 'fs.file-nr'], nice=19, wait=True)
     newresult = result['stdout'].decode('utf-8').strip().split(' ')[2].split('\t')
     return (newresult[0], newresult[2])
 
 
 async def getserveruptime():
+    """Summary
+
+    Returns:
+        TYPE: Description
+    """
     return elapsedTime(Now(), psutil.boot_time())
 
 
 async def getcpuload():
+    """Summary
+
+    Returns:
+        TYPE: Description
+    """
     rawcpuload = psutil.getloadavg()
     numcores = psutil.cpu_count()
     cpufreq = psutil.cpu_freq()[0] / 1000
@@ -194,6 +422,11 @@ async def getcpuload():
 
 
 async def getservermem():
+    """Summary
+
+    Returns:
+        TYPE: Description
+    """
     process = await asyncserverexec(['free', '-m'], nice=19, wait=True)
     lines = process['stdout'].decode().split('\n')
     memvalues = lines[1].strip().split()
@@ -205,6 +438,11 @@ async def getservermem():
 
 
 async def _procstats(inst):
+    """Summary
+
+    Args:
+        inst (TYPE): Description
+    """
     log.trace(f'Running process instances stats for {inst}')
     instpid = getinstpid(inst)
     if instpid == "CHANGEME":  # CHANGE ME
@@ -219,12 +457,25 @@ async def _procstats(inst):
 
 
 async def processinststats(instances):
+    """Summary
+
+    Args:
+        instances (TYPE): Description
+
+    Returns:
+        TYPE: Description
+    """
     for inst in instances:
         asyncio.create_task(_procstats(inst))
     return True
 
 
 async def processserverstats(instances):
+    """Summary
+
+    Args:
+        instances (TYPE): Description
+    """
     log.trace('Running process server stats')
     serveruptime = await getserveruptime()
     servermem = await getservermem()
@@ -236,6 +487,11 @@ async def processserverstats(instances):
 
 @log.catch
 def setarknice(inst):
+    """Summary
+
+    Args:
+        inst (TYPE): Description
+    """
     instpid = getinstpid(inst)
     if instpid is not None:
         proc = psutil.Process(getinstpid)
@@ -246,6 +502,20 @@ def setarknice(inst):
 
 @log.catch
 def serverexec(cmdlist, nice=10, null=False):
+    """
+    [Summary]
+
+    Args:
+        cmdlist (TYPE): [Description]
+        nice (int, [Optional]): [Description]
+        null (bool, [Optional]): [Description]
+
+    Returns:
+        TYPE: [Description]
+
+    Raises:
+        TypeError: [Description]
+    """
     if type(cmdlist) is not list:
         raise TypeError
     else:
