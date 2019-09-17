41c592c69dad7f7123029561445e6ac122103d09
Ian Perry <ianperry99@gmail.com>
2019-09-17 19:02:28 -0400

    update

--- web/api/api.py
+++ web/api/api.py
@@ -1,6 +1,41 @@
 from fastapi import FastAPI
+from modules.asyncdb import asyncDB
+from modules.redis import instancevar, globalvar, instancestate
 
 app = FastAPI()
+
+db = asyncDB()
+
+
+@app.on_event("startup")
+async def startup():
+    db = asyncDB()
+    await db.connect(min=1, max=5, timeout=60)
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await db.close()
+
+
+@app.get('/servers/status')
+async def servers_status():
+    instances = await globalvar.getlist('allinstances')
+    statuslist = ()
+    for inst in instances:
+        if instancevar.getint(inst, 'islistening') == 1:
+            if instancestate.check(inst, 'restartwaiting'):
+                status = 'restarting'
+            else:
+                status = 'online'
+        else:
+            status = 'offline'
+        statuslist = statuslist + ((status,))
+
+
+@app.get("/players/online")
+async def players_online():
+    return await db.fetchone(f"SELECT COUNT(*) FROM players WHERE online = True")
 
 
 @app.get("/")
