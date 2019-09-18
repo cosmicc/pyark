a234be24b9f59757f9da2d803d39806ad6a3e1a3
Ian Perry <ianperry99@gmail.com>
2019-09-17 21:27:57 -0400

    update

--- api.py
+++ api.py
@@ -1,10 +1,35 @@
+from functools import wraps
+
 from fastapi import FastAPI
 from modules.asyncdb import asyncDB
-from modules.redis import instancevar, globalvar, instancestate
+from modules.redis import globalvar, instancestate, instancevar
+from starlette.responses import Response
 
 app = FastAPI(openapi_prefix="/api")
 
 db = asyncDB()
+
+'''
+async def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        if 'X-API-KEY' in request.headers:
+            token = request.headers['X-API-KEY']
+        if not token:
+            apilog.warning(f'API request without a token')
+            return {'message': 'Not Authorized'}, 401
+        pkeys = dbquery("SELECT * FROM players WHERE apikey = '%s'" % (token,), fmt='dict', fetch='one')
+        if pkeys is None:
+            apilog.warning(f'API request invalid token: {token}')
+            return {'message': 'Invalid Token'}, 401
+        if pkeys['banned'] == 'True':
+            apilog.warning(f'API request from banned player: {pkeys[0]} token: {token}')
+            return {'message': 'You are Banned'}, 401
+        apilog.info(f'API request granted for player: {pkeys["playername"]} steamid: {pkeys["steamid"]}')
+        return f(*args, **kwargs)
+    return decorated
+'''
 
 
 @app.on_event("startup")
@@ -38,33 +63,38 @@ async def players_online():
     return await db.fetchone(f"SELECT COUNT(*) FROM players WHERE online = True")
 
 
-@app.get("/players/info")
-async def players_info(steamid=None, playername=None):
+@app.get("/players/info", status_code=200)
+async def players_info(steamid=None, playername=None, response: Response):
     if steamid:
         player = await db.fetchone(f"SELECT * FROM players WHERE steamid = '{steamid}'")
         if player:
             return {'player_info': player}
         else:
+            response.status_code = 400
             return {'message': 'steamid does not exist'}
     elif playername:
         player = await db.fetchone(f"SELECT * FROM players WHERE playername = '{playername}'")
         if player:
             return {'player_info': player}
         else:
+            response.status_code = 400
             return {'message': 'playername does not exist'}
     else:
+        response.status_code = 400
         return {'message': 'you must specify a steamid or playermame'}
 
 
-@app.get('/servers/info')
-async def servers_info(servername=None):
+@app.get('/servers/info', status_code=200)
+async def servers_info(servername=None, response: Response):
     instances = await globalvar.getlist('allinstances')
     if servername is not None:
         if servername in instances:
             return {'server_info': await db.fetchone(f"SELECT * FROM instances WHERE name = '{servername}'")}
         else:
+            response.status_code = 400
             return {'message': 'invalid server name'}
     else:
+        response.status_code = 400
         return {'message': 'you must specify a server name'}
 
 
