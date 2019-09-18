de2b18ed391883b7a6905c7008a637d4f59a8b9b
Ian Perry <ianperry99@gmail.com>
2019-09-18 00:03:47 -0400

    update

--- web/webui/views.py
+++ web/webui/views.py
@@ -57,7 +57,8 @@ def GameThread(sid, namespace):
             if msg is not None:
                 log.trace(f'Sending logline to: {sid}')
                 socketio.emit('gameline', {'line': msg}, namespace=namespace, room=sid)
-            sleep(.3)
+            else:
+                sleep(.3)
     log.debug(f'Closing down game log watch for {sid}')
 
 
@@ -74,7 +75,8 @@ def ChatThread(sid, namespace):
             if msg is not None:
                 log.trace(f'Sending logline to: {sid}')
                 socketio.emit('chatline', {'line': msg}, namespace=namespace, room=sid)
-            sleep(.3)
+            else:
+                sleep(.3)
     log.debug(f'Closing down chat log watch for {sid}')
 
 
@@ -91,7 +93,8 @@ def LogThread(sid, namespace):
             if msg is not None:
                 log.trace(f'Sending logline to: {sid}')
                 socketio.emit('logline', {'line': msg}, namespace=namespace, room=sid)
-            sleep(.3)
+            else:
+                sleep(.3)
     log.debug(f'Closing down pyark log watch for {sid}')
 
 
@@ -108,7 +111,8 @@ def DebugLogThread(sid, namespace):
             if msg is not None:
                 log.trace(f'Sending logline to: {sid}')
                 socketio.emit('logline', {'line': msg}, namespace=namespace, room=sid)
-            sleep(.3)
+            else:
+                sleep(.3)
     log.debug(f'Closing down debug pyark log watch for {sid}')
 
 
