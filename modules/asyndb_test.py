2d9313441bcdbea446103d1ffee6a81ad9b02147
Ian Perry <ianperry99@gmail.com>
2019-09-18 19:51:32 -0400

    update

--- modules/asyndb_test.py
+++ modules/asyndb_test.py
@@ -2,6 +2,7 @@ import pytest
 from loguru import logger as log
 
 
+@pytest.mark.order1
 async def test_connection(cursor):
     rs = cursor.execute('SELECT id FROM messages')
     assert len(rs) == 1
