c082f87c5d28d483076f5a1563cc0d62e3ce66d9
Ian Perry <ianperry99@gmail.com>
2019-09-18 23:44:02 -0400

    update

---
+++ misc/aredis_updates.py
@@ -0,0 +1,16 @@
+
+
+async def zpopmin(self, name, count):
+    """
+    Remove and return up to ``count`` members with the
+    lowest scores from the sorted set ``name``
+    """
+    return await self.execute_command('ZPOPMIN', name, count)
+
+
+async def zpopmax(self, name, count):
+    """
+    Remove and return up to ``count`` members with the
+    highest scores from the sorted set ``name``
+    """
+    return await self.execute_command('ZPOPMAX', name, count)
--- modules/timehelper_test.py
+++ modules/timehelper_test.py
@@ -11,7 +11,8 @@ def test_truncate_float():
     with pytest.raises(TypeError):
         timehelper.truncate_float(12.3531, 1.4)
         timehelper.truncate_float(12, 0)
-    assert timehelper.truncate_float('12a3531', 1)
+    with pytest.raises(ValueError):
+        timehelper.truncate_float('12a3531', 1)
     assert timehelper.truncate_float('12.3531', 1) == 12.3
     assert type(timehelper.truncate_float(12.23456, 2)) is float
     assert timehelper.truncate_float(12.34567, 2) == 12.34
