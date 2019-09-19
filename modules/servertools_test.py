ac204d45444f0fb1fb0b285b5d446e97bf4a37db
Ian Perry <ianperry99@gmail.com>
2019-09-19 00:32:46 -0400

    update

--- modules/servertools_test.py
+++ modules/servertools_test.py
@@ -21,11 +21,11 @@ async def test_getcpuload():
     assert type(results[0]) is int and type(results[1]) is float and type(results[2]) is float and type(results[3]) is float and type(results[4]) is float
 
 
-async def test_openfiles():
-    assert type(await modules.servertools.openfiles()) is tuple
-    assert await modules.servertools.openfiles() is not None
-    assert len(await modules.servertools.openfiles()) == 2
-    results = await modules.servertools.openfiles()
+async def test_getopenfiles():
+    assert type(await modules.servertools.getopenfiles()) is tuple
+    assert await modules.servertools.getopenfiles() is not None
+    assert len(await modules.servertools.getopenfiles()) == 2
+    results = await modules.servertools.getopenfiles()
     assert type(results[0]) is int and type(results[1]) is int
 
 """
