1d805df65a67d5bd80360d09b73df019cddef54c
Ian Perry <ianperry99@gmail.com>
2019-09-19 01:32:14 -0400

    update

---
+++ modules/gtranslate_test.py
@@ -0,0 +1,16 @@
+import sys
+
+import pytest
+import modules.gtranslate
+
+sys.path.append('/home/ark/pyark')
+
+
+def test_trans_to_eng():
+    with pytest.raises(TypeError):
+        assert type(modules.gtranslate.trans_to_eng(132))
+        assert type(modules.gtranslate.trans_to_eng(['Test', 'me']))
+    assert type(modules.gtranslate.trans_to_eng('hello')) is str
+    assert type(modules.gtranslate.trans_to_eng('hola')) == 'hello'
+
+
