7fd3890c8503e0cefadd952221cfafc11ed718af
Ian Perry <ianperry99@gmail.com>
2019-09-18 21:22:36 -0400

    update

--- conftest.py
+++ conftest.py
@@ -7,33 +7,3 @@ from modules.configreader import psql_db, psql_host, psql_port, psql_pw, psql_us
 
 sys.path.append('/home/ark/pyark')
 
-db = asyncDB()
-
-
-@pytest.fixture(scope="session")
-async def asyncdb():
-    await db.connect(min=1, max=10, timeout=30)
-    conn = await db._aquire()
-    yield conn
-    await db._release(conn)
-
-
-@pytest.fixture()
-async def asyncdb_cursor(conn):
-    cursor = await conn.cursor()
-    yield cursor
-    await conn.rollback()
-
-
-@pytest.fixture(scope='session')
-def conn():
-    conn = psycopg2.connect(dbname=psql_db, user=psql_user, host=psql_host, port=psql_port, password=psql_pw)
-    yield conn
-    conn.close()
-
-
-@pytest.fixture()
-def cursor(conn):
-    cursor = conn.cursor()
-    yield cursor
-    conn.rollback()
