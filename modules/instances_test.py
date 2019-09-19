765cceaa665da1aa33ec489252c6867e0413130c
Ian Perry <ianperry99@gmail.com>
2019-09-18 20:26:53 -0400

    update

--- modules/instances_test.py
+++ modules/instances_test.py
@@ -8,5 +8,7 @@ sys.path.append('/home/ark/pyark')
 
 def test_isinstanceup():
     with pytest.raises(TypeError):
-        pass
+        modules.instances.isinstanceup(None)
+        modules.instances.isinstanceup(1)
+        modules.instances.isinstanceup(['island', 'ragnarok'])
     assert modules.instances.isinstanceup('ragnarok') == '1'
