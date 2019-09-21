45ed5d23ffec6884eb3f6136d422addda3a3cd9b
Ian Perry <ianperry99@gmail.com>
2019-09-20 21:20:41 -0400

    update

--- web/app.py
+++ web/app.py
@@ -1,11 +1,30 @@
 from quart import Quart, websocket, redirect, render_template, url_for
+from modules.asyncdb import asyncDB
+from modules.redis import RedisClass
 
 app = Quart(__name__)
 
 
+@app.before_serving
+async def db_pool():
+    app.db = asyncDB()
+    await app.db.connect(min=3, max=20, timeout=240)
+
+
+@app.before_serving
+async def redis_pool():
+    Redis = RedisClass()
+    app.redis = Redis.redis
+
+
+@app.after_serving
+async def db_close():
+    await app.db.close()
+
+
 @app.route('/')
 async def index():
-    return await render_template('landing.html')
+    return await render_template('home.html')
 
 
 @app.websocket('/ws')
