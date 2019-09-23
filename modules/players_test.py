51a3bc26912893bdb2ddbe5c07878a0413c2409c
Ian Perry <ianperry99@gmail.com>
2019-09-23 00:21:49 -0400

    update

--- modules/players_test.py
+++ modules/players_test.py
@@ -8,17 +8,17 @@ sys.path.append('/home/ark/pyark')
 
 
 async def test_asyncgetplayerinfo():
-    assert await modules.players.asyncgetplayerinfo(steamid='76561198408657294') is Record
+    assert await type(modules.players.asyncgetplayerinfo(steamid='76561198408657294')) is Record
     player = await modules.players.asyncgetplayerinfo(steamid='76561198408657294')
     assert player['playername'] == 'admin'
     assert await len(modules.players.asyncgetplayerinfo(steamid='76561198408657294')) == 1
-    assert await modules.players.asyncgetplayerinfo(playername='Admin') is Record
+    assert await type(modules.players.asyncgetplayerinfo(playername='Admin')) is Record
     assert await len(modules.players.asyncgetplayerinfo(playername='Admin')) == 1
-    assert await modules.players.asyncgetplayerinfo(discordid='Rykker#7393') is Record
+    assert await type(modules.players.asyncgetplayerinfo(discordid='Rykker#7393')) is Record
     player = await modules.players.asyncgetplayerinfo(discordid='Rykker#7393')
     assert player['playername'] == 'rykker'
     assert await len(modules.players.asyncgetplayerinfo(discordid='Rykker#7393')) == 1
-    assert await modules.players.asyncgetplayerinfo(steamname='Galaxy-Cluster') is Record
+    assert await type(modules.players.asyncgetplayerinfo(steamname='Galaxy-Cluster')) is Record
     player = await modules.players.asyncgetplayerinfo(steamname='Galaxy-Cluster')
     assert player['playername'] == 'admin'
     assert await len(modules.players.asyncgetplayerinfo(steamname='Galaxy-Cluster')) == 1
