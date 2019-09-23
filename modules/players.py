13bd56f6d67b6792d581a9065f571611b4b22adf
Ian Perry <ianperry99@gmail.com>
2019-09-23 19:39:28 -0400

    update

--- modules/players.py
+++ modules/players.py
@@ -235,12 +235,12 @@ async def asyncgetplayersonline(instance: str, fmt: str='count'):
     elif fmt == 'string':
         players = []
         for player in iter(dbdata):
-            players.append(dbdata[player]['playername'])
+            players.append(player['playername'])
         return ', '.join(players)
     elif fmt == 'list':
         players = []
         for player in iter(dbdata):
-            players.append(dbdata[player]['playername'])
+            players.append(player['playername'])
         return players
 
 
