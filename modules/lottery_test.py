78b8f6ee46702df84d7114c134f916be1ee7b62d
Ian Perry <ianperry99@gmail.com>
2019-09-22 23:31:35 -0400

    update

--- modules/lottery_test.py
+++ modules/lottery_test.py
@@ -7,5 +7,5 @@ import pytest
 sys.path.append('/home/ark/pyark')
 
 
-def test_asyncgetlastlotteryinfo():
+async def test_asyncgetlastlotteryinfo():
     assert type(await modules.lottery.asyncgetlastlotteryinfo()) is Record
