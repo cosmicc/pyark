d78a8d544c5d6a54d819deda979d9f55d36cb7b4
Ian Perry <ianperry99@gmail.com>
2019-09-26 20:08:39 -0400

    update

--- modules/masterwatchdog.py
+++ modules/masterwatchdog.py
@@ -2,6 +2,7 @@ from ping3 import ping
 from modules.redis import redis
 from typing import Optional
 from loguru import logger as log
+from modules.timehelper import truncate_float
 
 
 async def pinghost(host: str) -> Optional[float]:
@@ -13,7 +14,8 @@ async def checkhosts():
     for instance in iter(instances):
         resp = pinghost(instance.decode())
         if resp:
+            log.debug(f'Ping response from {instance.decode()}: {truncate_float(resp, 2)}ms')
             if resp > 5:
-                log.warning('High internal ping times to [{instance.decode()}]')
+                log.warning(f'High internal ping times to [{instance.decode()}]')
         else:
-            log.warning('No reponse from host [{instance.decode()}]')
+            log.warning(f'No reponse from host [{instance.decode()}]')
