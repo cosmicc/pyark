9a2b38d7597251acf5c2e1fea6a72c88c3d85de5
Ian Perry <ianperry99@gmail.com>
2019-09-28 23:08:25 -0400

    update

--- modules/apihelper_test.py
+++ modules/apihelper_test.py
@@ -9,20 +9,6 @@ sys.path.append("/home/ark/pyark")
 
 
 @pytest.mark.last
-async def test_asyncfetchclusterauctiondata(testsession):
-    # assert (
-    #    type(await modules.apihelper.asyncfetchclusterauctiondata(testsession)) == dict
-    #)
-    nd = await modules.apihelper.asyncfetchclusterauctiondata(testsession)
-    pp = pprint.PrettyPrinter(indent=4)
-    lp = 0
-    for each in nd:
-        lp += 1
-        pp.pprint(each)
-    print(lp)
-
-'''
-@pytest.mark.last
 async def test_getsteaminfo(testsession):
     assert (
         await modules.apihelper.getsteaminfo("76561198408657294", "admin", testsession)
@@ -46,4 +32,4 @@ async def test_asyncfetchauctiondata(testsession):
         )
         is None
     )
-'''
+
