9edc118353cac7e1580de6ac04edf130369f1351
unknown <ianperry99@gmail.com>
2019-10-13 02:01:34 -0400

    update

--- api.py
+++ api.py
@@ -4,7 +4,7 @@ from modules.redis import globalvar, instancestate, instancevar, redis
 from modules.servertools import stripansi, asyncglobalbuffer
 from starlette.responses import Response
 
-app = FastAPI(openapi_prefix="/api")
+app = FastAPI(openapi_prefix="/api", title="Galaxy Cluster API", description="Ark: Galaxy Cluster Server API", version="2.0.1", redoc_url=None)
 
 db = asyncDB()
 
