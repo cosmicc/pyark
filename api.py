45449ebb3005eba4d70dc618c701cee020c6c60c
unknown <ianperry99@gmail.com>
2019-10-14 00:07:55 -0400

    update

--- api.py
+++ api.py
@@ -37,7 +37,7 @@ async def token_required(f):
 
 async def check_apikey(apikey: str = Depends(security)):
     keys = await db.fetchall("SELECT apikey from players WHERE apikey is not NULL")
-    if apikey not in keys.itervalues():
+    if apikey not in keys.values():
         raise HTTPException(status_code=HTTP_401_UNAUTHORIZED)
     return apikey
 
